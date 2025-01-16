6fa7d177ee64909bce5d13e1dd9769426534c903
Julie Segueni <83018837+JSegueni@users.noreply.github.com>
2025-01-16 11:42:58 +0100

    Delete scripts/fanc_insulation_score_after_cool_norm.sh

--- scripts/fanc_insulation_score_after_cool_norm.sh
+++
@@ -1,6 +0,0 @@
-# fanc insulation score calling
-
-
-fanc insulation NPC.iced.intra.cool NPC.iced.intra.ISfanc.bw -o bigwig -w 500000 && \
-fanc insulation ES.iced.intra.cool ES.iced.intra.ISfanc.bw -o bigwig -w 500000 && \
-fanc insulation CN.iced.intra.cool CN.iced.intra.ISfanc.bw -o bigwig -w 500000 
